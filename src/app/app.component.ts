6ef275e24afc7502b21653a1354b0f76335770f9
Anthony Schmieder <anthony@primatelabs.com>
2016-08-07 22:24:10 -0400

    Add a new component and reference it from AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { OtherComponent } from './other'
 
 @Component({
   moduleId: module.id,
   selector: 'app-root', // element selector. attribute selector is [sel] then e.g. <div sel> will be replaced
-  templateUrl: 'app.component.html',
+  template: `
+    <h1>blah</h1>
+    <app-other></app-other>
+  `,
   styleUrls: ['app.component.css'],
   styles: [`
   h1 {
     color: red
   }
-  `]
+  `],
+  directives: [OtherComponent]
 })
 export class AppComponent {
   title = 'blah';
