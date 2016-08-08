7de0d3fc3a57d53cbd0a801569c324e509b0bec8
Anthony Schmieder <anthony@primatelabs.com>
2016-08-07 22:14:25 -0400

    Add selector notes and inline style to default component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: 'app-root',
+  selector: 'app-root', // element selector. attribute selector is [sel] then e.g. <div sel> will be replaced
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  styles: [`
+  h1 {
+    color: red
+  }
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'blah';
 }
